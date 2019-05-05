3c01f32d6c2c51ea46b8908fff1577ffc7786f55
Dmitry <dimamik@yandex.ru>
2019-05-05 14:40:25 +0300

    Добавлен testbase, хелперы, менеджер хелперов, удален пустой тест

---
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        protected LoginHelper loginHelper;
+        protected NavigationHelper navigator;
+        protected GroupHelper groupHelper;
+
+        public ApplicationManager()
+        {
+            loginHelper = new LoginHelper(driver);
+            navigator = new NavigationHelper(driver, baseURL);
+            groupHelper = new GroupHelper(driver);
+
+            driver = new ChromeDriver();
+            baseURL = "http://localhost";
+        }
+
+        public void Stop ()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public LoginHelper Auth
+        {
+            get
+            {
+                return loginHelper;
+            }
+        }
+
+        public NavigationHelper Navigator
+        {
+            get
+            {
+                return navigator;
+            }
+        }
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            app.Navigator.OpenLoginPage();
+            app.Auth.Login(new AccountData("admin", "secret"));
+            app.Navigator.GoToGroupsPage();
+            app.Groups.InitGroupCreation();
+            GroupData group = new GroupData("Group name");
+            group.Header = "Header name";
+            group.Footer = "Footer name";
+            app.Groups.FillGroupForm(group);
+            app.Groups.SubmitGroupCreation();
+            app.Groups.ReturnToGroupsPage();
+            app.Auth.Logout();
+        }
+    }
+}
